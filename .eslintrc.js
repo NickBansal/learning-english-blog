1740ebaaaf7922be33bb360976fba2d8513ae87b
Nick Bansal <nick.bansal@autotrader.co.uk>
2023-11-07 15:20:32 +0000

    fixed the laoder function

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,38 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
-    },
-    extends: ["plugin:react/recommended", "standard-with-typescript"],
-    overrides: [
-        {
-            files: ["*.ts", "*.tsx"], // Your TypeScript files extension
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
 
-            // As mentioned in the comments, you should extend TypeScript plugins here,
-            // instead of extending them outside the `overrides`.
-            // If you don't want to extend any rules, you don't need an `extends` attribute.
-            extends: [
-                "plugin:@typescript-eslint/recommended",
-                "plugin:@typescript-eslint/recommended-requiring-type-checking"
-            ],
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+      ],
 
-            parserOptions: {
-                project: ["./tsconfig.json"] // Specify it only for TypeScript files
-            }
-        }
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-    },
-    plugins: ["react", "@typescript-eslint"],
-    rules: {
-        "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/quotes": 0,
-        "@typescript-eslint/semi": 0,
-        "@typescript-eslint/comma-dangle": 0,
-        "@typescript-eslint/space-before-function-paren": 0,
-        "@typescript-eslint/indent": "off"
+      parserOptions: {
+        project: ["./tsconfig.json"] // Specify it only for TypeScript files
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/quotes": 0,
+    "@typescript-eslint/semi": 0,
+    "@typescript-eslint/comma-dangle": 0,
+    "@typescript-eslint/space-before-function-paren": 0,
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/explicit-function-return-type": 0
+  }
 };
--- app/root.tsx
+++ app/root.tsx
@@ -1,36 +1,42 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderArgs } from "@remix-run/node";
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
 } from "@remix-run/react";
 import stylesheet from "~/tailwind.css";
+import { themeSessionResolver } from "./utils/session.server";
 
 export const links: LinksFunction = () => [
-    { rel: "stylesheet", href: stylesheet }
+  { rel: "stylesheet", href: stylesheet }
 ];
 
+export async function loader({ request }: LoaderArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+
+  return {
+    theme: getTheme()
+  };
+}
+
 export default function App(): JSX.Element {
-    return (
-        <html lang="en">
-            <head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width,initial-scale=1"
-                />
-                <Meta />
-                <Links />
-            </head>
-            <body>
-                <Outlet />
-                <ScrollRestoration />
-                <Scripts />
-                <LiveReload />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
