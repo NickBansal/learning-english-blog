d8cd6576efa88c95f9eab97bfad2d469252c5b23
Nick Bansal <nick.bansal@autotrader.co.uk>
2023-12-01 20:33:55 +0000

    fixed the styles for the iframes

--- app/components/mdx-components/components/iframe.tsx
+++ app/components/mdx-components/components/iframe.tsx
@@ -2,11 +2,9 @@ import { type IframeTypes } from '~/types/mdx-interface';
 
 const IFrame = ({ children, ...rest }: IframeTypes) => {
   return (
-    <div className="w-full mx-auto my-10">
-      <center>
-        <iframe {...rest}>{children}</iframe>
-      </center>
-    </div>
+    <iframe {...rest} className="max-w-full mx-auto my-10">
+      {children}
+    </iframe>
   );
 };
 
