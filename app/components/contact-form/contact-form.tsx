d0ebfc510b72428fd8f001d7db4f7fca81c844c4
Nick Bansal <nick.bansal@autotrader.co.uk>
2023-12-06 14:09:53 +0000

    added in a new contact card component

--- app/components/contact-form/contact-form.tsx
+++ app/components/contact-form/contact-form.tsx
@@ -4,18 +4,18 @@ import { SubmitButton } from '../buttons/buttons';
 
 export const ContactForm = () => {
   return (
-    <div className="w-full max-w-lg mx-auto mb-44">
-      <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="w-full max-w-lg mx-auto mb-28 mt-2 md:mt-16">
+      <Form className="bg-white dark:bg-gray-900 shadow-md dark:shadow-white rounded px-8 pt-6 pb-8 mb-4">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2"
               htmlFor="grid-first-name"
             >
               First Name
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-white border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               id="grid-first-name"
               type="text"
               placeholder="Jane"
@@ -23,13 +23,13 @@ export const ContactForm = () => {
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2"
               htmlFor="grid-last-name"
             >
               Last Name
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-white border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
               type="text"
               placeholder="Doe"
@@ -39,13 +39,13 @@ export const ContactForm = () => {
         <div className="flex flex-col -mx-3 mb-6">
           <div className="w-full px-3 mb-6 md:mb-0">
             <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2"
               htmlFor="grid-first-email"
             >
               Email
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-8 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-white border rounded py-3 px-4 mb-8 leading-tight focus:outline-none focus:bg-white"
               id="grid-first-email"
               type="email"
               placeholder="janedoe@email.com"
@@ -53,13 +53,13 @@ export const ContactForm = () => {
           </div>
           <div className="w-full px-3">
             <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-700 dark:text-white text-xs font-bold mb-2"
               htmlFor="grid-comments"
             >
               Comments
             </label>
             <textarea
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 dark:text-white border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-comments"
               placeholder="Whats on your mind?"
               rows={7}
