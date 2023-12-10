29660dadbb23275c0b11d5c878ced207274ba8ab
Nick Bansal <nick.bansal@autotrader.co.uk>
2023-12-10 11:57:55 +0000

    edited the og material

--- app/constants/META_DATA.ts
+++ app/constants/META_DATA.ts
@@ -1,10 +1,10 @@
 interface openGraphTypes {
   content?: string;
   description: string;
-  title?: string;
+  title: string;
 }
 
-const openGraph = ({ content = 'website', description, title = 'English Everyday' }: openGraphTypes) => {
+const openGraph = ({ content = 'website', description, title }: openGraphTypes) => {
   return [
     {
       property: 'og:type',
@@ -16,7 +16,7 @@ const openGraph = ({ content = 'website', description, title = 'English Everyday
     },
     {
       property: 'og:title',
-      content: title
+      content: `English Everyday: ${title}`
     },
     {
       property: 'og:description',
@@ -44,19 +44,21 @@ export const homePage = [
       'Explore insightful articles on English Everyday in our blog. Get expert tips, guides, and resources to help with mastering the English language. Stay updated with the latest trends and valuable insights from our skilled Language coach. Dive into a wealth of information to be confident with speaking English today!'
   },
   ...openGraph({
+    title: 'Home Page',
     description:
       'Explore insightful articles on English Everyday in our blog. Get expert tips, guides, and resources to help with mastering the English language. Stay updated with the latest trends and valuable insights from our skilled Language coach. Dive into a wealth of information to be confident with speaking English today!'
   })
 ];
 
 export const allBlogs = [
-  { title: 'English Everyday | content' },
+  { title: 'English Everyday | Content' },
   {
     name: 'description',
     content:
       'Explore insightful articles on English Everyday in our blog. Get expert tips, guides, and resources to help with mastering the English language. Stay updated with the latest trends and valuable insights from our skilled Language coach. Dive into a wealth of information to be confident with speaking English today!'
   },
   ...openGraph({
+    title: 'All content',
     description:
       'Explore insightful articles on English Everyday in our blog. Get expert tips, guides, and resources to help with mastering the English language. Stay updated with the latest trends and valuable insights from our skilled Language coach. Dive into a wealth of information to be confident with speaking English today!'
   })
@@ -68,7 +70,7 @@ export const singleBlog = (title: string, content: string) => [
     name: 'description',
     content
   },
-  ...openGraph({ content: 'article', description: content, title: `English Everyday: ${title}` })
+  ...openGraph({ title, content: 'article', description: content })
 ];
 
 export const aboutPage = [
@@ -79,6 +81,7 @@ export const aboutPage = [
       'Discover the story behind English Everyday. Learn about our mission, values, and commitment to educating English language. Explore how our dedicated team brings English coaching to all foreign students. Get to know us and our passion for Language coaching!'
   },
   ...openGraph({
+    title: 'About Us',
     description:
       'Discover the story behind English Everyday. Learn about our mission, values, and commitment to educating English language. Explore how our dedicated team brings English coaching to all foreign students. Get to know us and our passion for Language coaching!'
   })
@@ -92,19 +95,21 @@ export const contactPage = [
       'Discover the story behind English Everyday. Learn about our mission, values, and commitment to educating English language. Explore how our dedicated team brings English coaching to all foreign students. Get to know us and our passion for Language coaching!'
   },
   ...openGraph({
+    title: 'Contact Us',
     description:
       'Discover the story behind English Everyday. Learn about our mission, values, and commitment to educating English language. Explore how our dedicated team brings English coaching to all foreign students. Get to know us and our passion for Language coaching!'
   })
 ];
 
 export const coursesPage = [
-  { title: 'English Everyday | All Our Courses' },
+  { title: 'English Everyday | Courses' },
   {
     name: 'description',
     content:
       'Discover the story behind English Everyday. Learn about our mission, values, and commitment to educating English language. Explore how our dedicated team brings English coaching to all foreign students. Get to know us and our passion for Language coaching!'
   },
   ...openGraph({
+    title: 'Courses',
     description:
       'Discover the story behind English Everyday. Learn about our mission, values, and commitment to educating English language. Explore how our dedicated team brings English coaching to all foreign students. Get to know us and our passion for Language coaching!'
   })
