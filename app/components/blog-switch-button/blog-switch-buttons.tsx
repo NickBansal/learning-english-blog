0d1f8ee6e3c47ab014661df3fd86f03968cab6be
Nick Bansal <nick.bansal@autotrader.co.uk>
2024-02-22 16:06:02 +0000

    cleaned up the blog siwtcher code

--- app/components/blog-switch-button/blog-switch-buttons.tsx
+++ app/components/blog-switch-button/blog-switch-buttons.tsx
@@ -1,25 +1,27 @@
 import classNames from 'classnames';
 
 export const BlogSwitcher = ({ videoCourse }: { videoCourse: boolean }) => {
+  const btnClasses =
+    'text-sm md:text-base mr-2 text-teal-600 dark:text-teal-300 dark:hover:text-teal-500 p-1 md:p-2 rounded-md';
+  const selectedBtn = 'border-2 border-gray-500 bg-teal-600 text-white dark:bg-gray-500 dark:border-gray-200';
+  const notSelectedBtn = 'border-0 hover:text-teal-700';
+
   return (
-    <div className=" border-b-2 border-gray-400 pb-4 -mt-4">
+    <div className=" border-b-2 border-gray-400 pb-2 md:pb-4 md:-mt-4 -mt-6">
       <a
         href="/blogs?$blogs=true"
-        className={classNames(
-          'mr-2 text-teal-600 hover:text-teal-700 dark:text-teal-300 dark:hover:text-teal-500 p-2 rounded-md',
-          {
-            'border-2 border-gray-500 bg-teal-600 text-white dark:bg-gray-500 dark:border-gray-200': !videoCourse,
-            'border-0 hover:text-teal-700': videoCourse
-          }
-        )}
+        className={classNames(btnClasses, {
+          [selectedBtn]: !videoCourse,
+          [notSelectedBtn]: videoCourse
+        })}
       >
         Blogs
       </a>
       <a
         href="/blogs?$videoCourse=true"
-        className={classNames('mr-2 text-teal-600  dark:text-teal-300 dark:hover:text-teal-500 p-2 rounded-md', {
-          'border-2 bg-teal-600 text-white border-gray-500 dark:bg-gray-500 dark:border-gray-200': videoCourse,
-          'border-0 hover:text-teal-700': !videoCourse
+        className={classNames(btnClasses, {
+          [selectedBtn]: videoCourse,
+          [notSelectedBtn]: !videoCourse
         })}
       >
         Video Courses
