6420455fe8f7532423211bb28f360ce8e36b208a
Nick Bansal <nick.bansal@autotrader.co.uk>
2024-02-23 16:12:23 +0000

    updated more button stylea

--- app/components/content-switch-button/content-switch-buttons.tsx
+++ app/components/content-switch-button/content-switch-buttons.tsx
@@ -2,11 +2,11 @@ import { Link } from '@remix-run/react';
 import classNames from 'classnames';
 
 export const ContentSwitcher = ({ videoContent }: { videoContent: boolean }) => {
-  const btnClasses =
-    'text-sm md:text-base text-teal-600 dark:text-teal-300 dark:hover:text-teal-500 h-full p-1 md:p-2 mb-2 rounded-md';
+  const btnClasses = 'text-sm md:text-base   h-full p-1 md:p-2 mb-2 rounded-md';
   const selectedBtn =
-    'border-2 border-gray-500 bg-teal-600 text-white dark:bg-gray-500 dark:hover:text-teal-300 dark:border-gray-200';
-  const notSelectedBtn = 'hover:text-gray-200 hover:bg-teal-500 dark:hover:bg-gray-300 dark:hover:text-gray-500';
+    'border-2 border-gray-500 bg-teal-600 text-white dark:bg-gray-500 dark:text-gray-300 dark:border-gray-200';
+  const notSelectedBtn =
+    'text-teal-600 dark:text-teal-300 hover:text-gray-200 hover:bg-teal-500 dark:hover:bg-gray-300 dark:hover:text-gray-500';
 
   return (
     <div className="flex border-b-2 border-gray-400 mb-4 md:mb-0 mt-2 space-x-1 items-center">
